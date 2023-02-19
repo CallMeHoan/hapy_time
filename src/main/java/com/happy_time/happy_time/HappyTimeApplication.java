06965b7597541b83706c72edda05648c94bfed47
ThienHoanNeee <softteam2021@gmail.com>
2023-02-19 16:05:47 +0700

    update

--- src/main/java/com/happy_time/happy_time/HappyTimeApplication.java
+++ src/main/java/com/happy_time/happy_time/HappyTimeApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableScheduling
 @SpringBootApplication
 @ComponentScan("com.happy_time.happy_time.*")
 public class HappyTimeApplication {
---
+++ src/main/java/com/happy_time/happy_time/ddd/schedule/ScheduleApplication.java
@@ -0,0 +1,19 @@
+package com.happy_time.happy_time.ddd.schedule;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class ScheduleApplication {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Scheduled(cron = "15 * * * * ?")
+    public void scheduleTaskWithInitialDelay() {
+        LOGGER.info("Test log on server");
+    }
+}
