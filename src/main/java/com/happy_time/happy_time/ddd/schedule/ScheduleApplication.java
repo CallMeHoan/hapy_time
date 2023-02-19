07b5ad612b7bc15be3950cb42dafd29622a065ef
ThienHoanNeee <softteam2021@gmail.com>
2023-02-19 16:18:49 +0700

    upate

--- src/main/java/com/happy_time/happy_time/ddd/schedule/ScheduleApplication.java
+++ src/main/java/com/happy_time/happy_time/ddd/schedule/ScheduleApplication.java
@@ -12,7 +12,7 @@ import javax.annotation.PostConstruct;
 public class ScheduleApplication {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    @Scheduled(cron = "* 0 0 ? * * *")
+    @Scheduled(cron = "0 0 0 ? * * *")
     public void scheduleTaskWithInitialDelay() {
         LOGGER.info("Chào Ngày Mới <3");
     }
