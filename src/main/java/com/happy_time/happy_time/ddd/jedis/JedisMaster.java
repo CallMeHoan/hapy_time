4d5fd8bcf37642214b51facf0e22d38c02bf5213
ThienHoanNeee <softteam2021@gmail.com>
2023-05-22 10:51:55 +0700

    update

---
+++ src/main/java/com/happy_time/happy_time/ddd/jedis/JedisMaster.java
@@ -0,0 +1,47 @@
+package com.happy_time.happy_time.ddd.jedis;
+
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Connection;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisException;
+
+import java.util.Map;
+
+@Component
+public class JedisMaster {
+
+    @Autowired
+    private Environment env;
+    public Jedis connect() {
+        try {
+            String connectionString = env.getProperty("redis.uri");
+            return new Jedis(connectionString);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Connect to redis failed");
+        }
+    }
+
+    public String hget(String key) {
+        Jedis jedis = this.connect();
+        return jedis.get(key);
+    }
+
+    public Map<String, String> hgetAll(String key) {
+        Jedis jedis = this.connect();
+        return jedis.hgetAll(key);
+    }
+
+    public Long hSet(String key, String field,String value) {
+        Jedis jedis = this.connect();
+        return jedis.hset(key, field, value);
+    }
+
+    public Long hSetAll(String key, Map<String, String> values) {
+        Jedis jedis = this.connect();
+        return jedis.hset(key, values);
+    }
+}
