86c98dcc47e40d4c3b566ae294ffd57c749f4044
ThienHoanNeee <softteam2021@gmail.com>
2023-05-23 09:03:27 +0700

    test redis

--- src/main/java/com/happy_time/happy_time/HappyTimeApplication.java
+++ src/main/java/com/happy_time/happy_time/HappyTimeApplication.java
@@ -8,12 +8,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 @ComponentScan("com.happy_time.happy_time.*")
 public class HappyTimeApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("user.timezone", "Asia/Ho_Chi_Minh");
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(HappyTimeApplication.class, args);
 	}
 
