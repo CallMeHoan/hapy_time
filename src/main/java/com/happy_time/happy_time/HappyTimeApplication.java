3e30fdb685c5fed0f112d36557071ac14107b7c7
ThienHoanNeeeee <hoannt@vihatgroup.com>
2023-05-28 15:19:42 +0700

    add

--- src/main/java/com/happy_time/happy_time/HappyTimeApplication.java
+++ src/main/java/com/happy_time/happy_time/HappyTimeApplication.java
@@ -2,6 +2,7 @@ package com.happy_time.happy_time;
 
 import com.happy_time.happy_time.common.DateTimeUtils;
 import com.happy_time.happy_time.common.HAPStringUtils;
+import com.happy_time.happy_time.ddd.tool.JobTool;
 import com.happy_time.happy_time.ddd.tool.ShiftResultTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
---
+++ src/main/java/com/happy_time/happy_time/ddd/tool/JobTool.java
@@ -0,0 +1,22 @@
+package com.happy_time.happy_time.ddd.tool;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobTool {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+
+    public void deleteShiftResult(String tenant_id) {
+        org.springframework.data.mongodb.core.query.Query query = new Query();
+        query.addCriteria(Criteria.where("is_deleted").is(false));
+        query.addCriteria(Criteria.where("tenant_id").is(tenant_id));
+        mongoTemplate.remove(query, "job");
+
+    }
+}
